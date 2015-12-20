8b6b734569e305c53d8971b6ff524ac4e60dd512
Mattia Barbon <mbarbon@users.sourceforge.net>
2015-12-20 15:02:53 +0000

    Import of MBARBON/DBGp-Client-0.06.tar.gz from CPAN.
    gitpan-cpan-distribution: DBGp-Client
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         MBARBON/DBGp-Client-0.06.tar.gz
    gitpan-cpan-author:       MBARBON
    gitpan-cpan-maturity:     released

--- lib/DBGp/Client/Response/Redirect.pm
+++ lib/DBGp/Client/Response/Redirect.pm
@@ -1,11 +1,11 @@
-package DBGp::Client::Response::BreakpointRemove;
+package DBGp::Client::Response::Redirect;
 
 use strict;
 use warnings;
 use parent qw(DBGp::Client::Response::Simple);
 
 __PACKAGE__->make_attrib_accessors(qw(
-    transaction_id command
+    transaction_id command success
 ));
 
 1;
